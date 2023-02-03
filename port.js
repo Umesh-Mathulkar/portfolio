c69c62ac2c291875d235f7456835669466a85f28
Umesh <mathulkar98@gmail.com>
2023-02-03 17:00:08 +0600

    updated04

--- port.js
+++ port.js
@@ -4,69 +4,18 @@ function namess() {
     document.getElementById("input").classList.add("hide");
 }
 
-function projects(element){
-    document.getElementById(element).classList.remove("hide")
+function display(){
+    let welcome = document.getElementById("welcome");
+    let mainShow = document.getElementById("mainShow")
+    welcome.classList.add("hide")
+    mainShow.classList.remove("hide");
 }
+const timeout = setTimeout(display,2400)
 
-function projectsDel(element){
-    document.getElementById(element).classList.add("hide")
+function display2(){
+    let skillIntro = document.getElementById("skillIntro");
+    let skiImg = document.getElementById("skiImg")
+    skillIntro.classList.add("hide")
+    skiImg.classList.remove("hide");
 }
-
-function pro(element){
-    if(element=="law"){
-    document.getElementById("calculator1").classList.add("hide");
-    document.getElementById("calculator2").classList.add("hide");
-    document.getElementById("ecommerce1").classList.add("hide")
-    document.getElementById("ecommerce2").classList.add("hide")
-}
-
-else if(element=="calc"){
-    document.getElementById("law1").classList.add("hide");
-    document.getElementById("law2").classList.add("hide");
-    document.getElementById("ecommerce1").classList.add("hide")
-    document.getElementById("ecommerce2").classList.add("hide")
-
-}
-
-else if(element=="commerce"){
-    document.getElementById("law1").classList.add("hide");
-    document.getElementById("law2").classList.add("hide");
-    document.getElementById("calculator1").classList.add("hide")
-    document.getElementById("calculator2").classList.add("hide")
-
-
-}
-
-}
-
-function proDel(element){
-    if (element=="law"){
-    document.getElementById("calculator1").classList.remove("hide");
-    document.getElementById("calculator2").classList.remove("hide");
-    document.getElementById("ecommerce1").classList.remove("hide")
-    document.getElementById("ecommerce2").classList.remove("hide")
-}
-
-else if(element=="calc"){
-    document.getElementById("law1").classList.remove("hide");
-    document.getElementById("law2").classList.remove("hide");
-    document.getElementById("ecommerce1").classList.remove("hide")
-    document.getElementById("ecommerce2").classList.remove("hide")
-
-}
-
-else if(element=="commerce"){
-    document.getElementById("law1").classList.remove("hide");
-    document.getElementById("law2").classList.remove("hide");
-    document.getElementById("calculator1").classList.remove("hide")
-    document.getElementById("calculator2").classList.remove("hide")
-
-
-}
-
-
-
-}
-
-
-
+const timeout2 = setTimeout(display2,10000)
