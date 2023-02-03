638b86cdbed1b71db0d164eee75f412687ce0ba2
Umesh <mathulkar98@gmail.com>
2023-02-03 17:24:26 +0600

    dark mode

--- port.js
+++ port.js
@@ -4,18 +4,41 @@ function namess() {
     document.getElementById("input").classList.add("hide");
 }
 
-function display(){
+function display() {
     let welcome = document.getElementById("welcome");
     let mainShow = document.getElementById("mainShow")
     welcome.classList.add("hide")
     mainShow.classList.remove("hide");
 }
-const timeout = setTimeout(display,2400)
+const timeout = setTimeout(display, 2400)
 
-function display2(){
+function display2() {
     let skillIntro = document.getElementById("skillIntro");
     let skiImg = document.getElementById("skiImg")
     skillIntro.classList.add("hide")
     skiImg.classList.remove("hide");
 }
-const timeout2 = setTimeout(display2,10000)
+const timeout2 = setTimeout(display2, 10000)
+
+function darkMode(element) {
+    let sun = document.getElementById("sun");
+    let moon = document.getElementById("moon");
+    let text = [document.getElementById("info"), document.getElementById("skills"), document.getElementById("projects"), document.getElementById("contact"),document.getElementById("footer")]
+    if (element == "moon") {
+        moon.classList.add("moon");
+        sun.classList.remove("sun");
+        document.body.classList.add("darkMode");
+        text.map((item) => {
+            item.classList.add("darkModeFont")
+        })
+    }
+    else {
+        sun.classList.add("sun");
+        moon.classList.remove("moon");
+        document.body.classList.remove("darkMode");
+        text.map((item) => {
+            item.classList.remove("darkModeFont")
+        }
+        )
+    }
+}
