f35af4ff7b2ea7c00c064ebeaf5661a74092c8c2
Umesh <mathulkar98@gmail.com>
2023-02-15 18:10:07 +0530

    updated dark mode

--- port.js
+++ port.js
@@ -20,25 +20,36 @@ function display2() {
 }
 const timeout2 = setTimeout(display2, 10000)
 
-function darkMode(element) {
-    let sun = document.getElementById("sun");
-    let moon = document.getElementById("moon");
+// function darkMode(element) {
+//     let sun = document.getElementById("sun");
+//     let moon = document.getElementById("moon");
+//     let text = [document.getElementById("info"), document.getElementById("skills"), document.getElementById("projects"), document.getElementById("contact"),document.getElementById("footer"),document.getElementById("hobby")]
+//     if (element == "moon") {
+//         moon.classList.add("moon");
+//         sun.classList.remove("sun");
+//         document.body.classList.add("darkMode");
+//         text.map((item) => {
+//             item.classList.add("darkModeFont")
+//         })
+//     }
+//     else {
+//         sun.classList.add("sun");
+//         moon.classList.remove("moon");
+//         document.body.classList.remove("darkMode");
+//         text.map((item) => {
+//             item.classList.remove("darkModeFont")
+//         }
+//         )
+//     }
+// }
+
+toggle=()=>{
     let text = [document.getElementById("info"), document.getElementById("skills"), document.getElementById("projects"), document.getElementById("contact"),document.getElementById("footer"),document.getElementById("hobby")]
-    if (element == "moon") {
-        moon.classList.add("moon");
-        sun.classList.remove("sun");
-        document.body.classList.add("darkMode");
-        text.map((item) => {
-            item.classList.add("darkModeFont")
-        })
-    }
-    else {
-        sun.classList.add("sun");
-        moon.classList.remove("moon");
-        document.body.classList.remove("darkMode");
-        text.map((item) => {
-            item.classList.remove("darkModeFont")
-        }
-        )
-    }
+    document.body.classList.toggle("dark");
+    document.getElementById("circle").classList.toggle("circleAnim")
+    document.getElementById("off").classList.toggle("hide")
+    document.getElementById("on").classList.toggle("hide")
+    text.map((item) => {
+        item.classList.toggle("darkModeFont")
+               })
 }
